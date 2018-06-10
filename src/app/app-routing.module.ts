49d7b1f223e3cb6ab3760b6d8f09b5dcf7fd8b98
abenjamin1313 <abenjamin1313@gmail.com>
2018-06-10 13:39:48 -0400

    Basic Ang5 app setup. Home COM, Features COM, Pricing COM, Blog COM, Team Com, Contact COM.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { NotFoundComponentComponent } from './components/not-found-component/not-found-component.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { TeamComponent } from './components/team/team.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponentComponent },
+  { path: 'home', component: HomeComponentComponent },
+  { path: 'features', component: FeaturesComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'pricing', component: PricingComponent },
+  { path: 'team', component: TeamComponent },
+  { path: '**', component: NotFoundComponentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler, NO_ERRORS_SCHEMA } from '@angular/core';
 
 // Material Design
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { MDBBootstrapModule, WavesModule  } from 'angular-bootstrap-md';
 // routing
 import { AppRoutingModule } from './app-routing.module';
 // components 
 import { AppComponent } from './app.component';
 // service
 import { ErrorHandlerService } from './service/error-handler.service';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { NotFoundComponentComponent } from './components/not-found-component/not-found-component.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { TeamComponent } from './components/team/team.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PricingComponent } from './components/pricing/pricing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent,
+    NotFoundComponentComponent,
+    FeaturesComponent,
+    BlogComponent,
+    TeamComponent,
+    ContactComponent,
+    PricingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    WavesModule 
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   providers: [
